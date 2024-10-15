3b91ce612f982c7ae7b2475e479ec44c688a1a5b
DanPorter <d.g.porter@hotmail.co.uk>
2024-10-15 13:48:17 +0100

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+
+import { GetHdfMap } from './hdfmap_components'
+ 
+
+function App() {
+ 
+  return (
+    <div style={{ padding: '20px',  width: '100%'}}>
+      <h1>Very Simple Scan Reader</h1>
+      <GetHdfMap />
+    </div>
+  );
+}
+ 
+export default App;
+
---
+++ frontend/src/MarkdownTextBox.tsx
@@ -0,0 +1,11 @@
+
+// https://react.dev/reference/react-dom/components/textarea
+
+import { Remarkable } from 'remarkable';
+
+const md = new Remarkable();
+
+export default function MarkdownPreview({ markdown }: { markdown: string }) {
+  const renderedHTML = md.render(markdown);
+  return <div dangerouslySetInnerHTML={{__html: renderedHTML}} />;
+}
